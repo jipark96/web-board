e39f814076fdfb8bce6d99eb2fbd17ac1e4ad5f3
jipark96 <wlgus996@naver.com>
2023-01-09 09:48:01 +0900

    처음부터 다시 시작

--- src/main/java/study/webboard/board/entity/Company.java
+++ src/main/java/study/webboard/board/entity/Company.java
@@ -1,22 +1,31 @@
 package study.webboard.board.entity;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-@Data
+@ToString
+@Getter
+@Table(name="company")
+@RequiredArgsConstructor
 public class Company {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    private String position;
+    @Column(name = "com_id")
+    private Long id;
+    @Column(name = "com_name")
     private String name;
-    private Integer money;
-    private String content;
-    private String tech;
+
+    @Column(name = "com_country")
+    private String country;
+
+    @Column(name = "com_area")
+    private String area;
+
+
+
 }
